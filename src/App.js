743e16fe5721fa0734b665bc247552f7766da687
Josh Gamache <josh_gamache@tcenergy.com>
2019-08-30 13:52:45 -0600

    testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import Box from './components/Box.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Box />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Box.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Box = () => {
+  let wid = 10;
+  let hei = 20;
+
+  return (<Rect width="${wid}" height="${hei}" x="50" y="20" />);
+}
+
+
+export default Box;
