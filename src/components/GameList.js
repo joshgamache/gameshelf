32c1130c4c7ec1b63231031ee5cecbd7945a7787
Josh Gamache <jsgamache@gmail.com>
2019-09-18 07:45:26 -0600

    Clean up GameList with proper .map convention

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -2,22 +2,23 @@ import React from 'react';
 import GameDisplay from './GameDisplay.js';
 
 const GameList = ({games}) => {
+  const gameComponents = games.map((game, i) => {
+    return (
+      <GameDisplay key = {i}
+        id = {game.id}
+        name = {game.name}
+        image = {game.image_url}
+        size_x = {game.size_height}
+        size_y = {game.size_width}
+        size_z = {game.size_depth}
+        size_units = {game.size_units}
+      />
+    )
+  });
+
   return (
     <div className = "columns is-multiline">
-      {
-        games.map((game, i) => {
-          return ( <
-            GameDisplay key = { i }
-            id = { games[i].id }
-            name = { games[i].name }
-            image = { games[i].image_url }
-            size_x = { games[i].size_height }
-            size_y = { games[i].size_width }
-            size_z = { games[i].size_depth }
-            size_units = { games[i].size_units }
-          />);
-        })
-      }
+      {gameComponents}
     </div>
   )
 }
