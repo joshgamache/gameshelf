187078f12568d5c6a6a805cdd2323baf8cb30e06
Josh Gamache <jsgamache@gmail.com>
2019-09-25 14:07:10 -0600

    Start logic to filter/sort games

---
+++ src/components/BoxSizeFitter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/*
+What does this function need to do?
+Take in an array of game boxes and the dimensions of the container
+Output two arrays: one with the "fit" game boxes and one with the "remaining" game boxes
+
+-- Is this the best way to handle the sorting?
+
+*/
+const BoxSizeFitter = () => {
+
+}
+
+export default BoxSizeFitter;
