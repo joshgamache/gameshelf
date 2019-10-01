c53201a135d6d7eca43e4d080b1c1f70c273356a
Josh Gamache <jsgamache@gmail.com>
2019-10-01 07:40:27 -0600

    Logic to determine if games fit on shelf

--- src/components/BoxSizeFitter.js
+++ src/components/BoxSizeFitter.js
@@ -8,8 +8,30 @@ Output two arrays: one with the "fit" game boxes and one with the "remaining" ga
 -- Is this the best way to handle the sorting?
 
 */
-const BoxSizeFitter = () => {
 
+// shelfBayDimensions = {width, height}
+
+const BoxSizeFitter = (incomingGamesArray, shelfBayDimensions) => {
+	// Takes in an array, object with dimensions of container
+	const gamesThatFit = [];
+	const gamesTooBig = [];
+
+	const shelfHeight = shelfBayDimensions.height;
+	let totalSize = shelfBayDimensions.width;
+
+	for (const game of incomingGamesArray){
+		const sortedGameShapeArray = [game.size_depth, game.size_width, game.size_height].sort((a, b) => a - b);
+
+		// check if a game can fit. If so, add it to the shelf, reduce the shelf size remaining. Otherwise bump it to the too-big array.
+		if (sortedGameShapeArray[0] <= totalSize && sortedGameShapeArray[1] <= shelfHeight){
+			gamesThatFit.push(game);
+			totalSize -= sortedGameShapeArray[0];
+		} else {
+			gamesTooBig.push(game);
+		}
+	}
+
+	return gamesThatFit, gamesTooBig;
 }
 
 export default BoxSizeFitter;
--- src/components/ShelfBay.js
+++ src/components/ShelfBay.js
@@ -21,46 +21,6 @@ const ShelfBay = (props) => {
     "h" : props.individualShelfArea.individualShelfHeight,
   }
 
-  const sg = {
-    "name" : "Sushi Go!",
-    "y" : 106.68,
-    "z" : 147.32,
-    "x" : 38.1,
-    "color" : "red"
-  }
-
-  const ctan = {
-    "name" : "Catan",
-    "x" : 294.64,
-    "y" : 241.3,
-    "z" : 76.2,
-    "color" : "blue"
-  }
-
-  const cnames = {
-    "name" : "Codenames",
-    "x" : 71.12,
-    "y" : 160.02,
-    "z" : 228.6,
-    "color" : "orange"
-  }
-
-  const cob = {
-    "name" : "Castles of Burgundy",
-    "x" : 309.88,
-    "y" : 215.9,
-    "z" : 71.12,
-    "color" : "brown"
-  }
-
-  const NF = {
-    "name" : "Near and Far",
-    "x" : 292.1,
-    "y" : 71.12,
-    "z" : 292.1,
-    "color" : "yellow"
-  }
-
   const gameBoxList = props.gamesToAddToShelf;
 
   const getColors = (urlForImage) => {
